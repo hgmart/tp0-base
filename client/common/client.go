794df8bf8aacbcc41def43cb17b19f4ff701efa1
Martin Hernan Gomez <mhgomez@fi.uba.ar>
2024-09-02 14:47:45 -0300

    Cierra el socket del cliente ante diferentes situaciones.

--- client/common/client.go
+++ client/common/client.go
@@ -55,11 +55,90 @@ func (c *Client) createClientSocket() error {
 	return nil
 }
 
+func (c *Client) closeSocketConnection() {
+	err := c.conn.Close()
+	if err != nil {
+		log.Errorf("action: closing_connection | result: fail | client_id: %v | error: %v",
+			c.config.ID,
+			err,
+		)
+	}
+}
+
+func (c *Client) waitServerReponse(signals chan os.Signal) {
+	if !c.isEnabled {
+		return
+	}
+
+	ch_msg := make(chan string)
+	ch_err := make(chan error)
+
+	go func() {
+		log.Infof("action: waiting_message | result: success | client_id: %v | msg: waiting echo message",
+			c.config.ID,
+		)
+
+		msg, err := bufio.NewReader(c.conn).ReadString('\n')
+		if err != nil {
+			ch_err <- err
+			return
+		}
+
+		ch_msg <- msg
+	}()
+
+	select {
+	case msg := <-ch_msg:
+		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
+			c.config.ID,
+			msg,
+		)
+
+	case err := <-ch_err:
+		log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+			c.config.ID,
+			err,
+		)
+
+	case received_signal := <-signals:
+		log.Infof("action: sigterm_signal | result: success | client_id: %v | msg: %v signal received",
+			c.config.ID,
+			received_signal,
+		)
+
+		c.isEnabled = false
+	}
+}
+
+func (c *Client) delayMessageDelivery(signals chan os.Signal) {
+	if !c.isEnabled {
+		return
+	}
+
+	select {
+	// Waits a time between sending one message and the next one
+	case currentTime := <-time.After(c.config.LoopPeriod):
+		log.Infof("action: sleep_time | result: success | client_id: %v | msg: awaiked at %v",
+			c.config.ID,
+			currentTime,
+		)
+
+		c.isEnabled = true
+
+	case received_signal := <-signals:
+		log.Debugf("action: sigterm_signal | result: success | client_id: %v | msg: %v signal received",
+			c.config.ID,
+			received_signal,
+		)
+
+		c.isEnabled = false
+	}
+}
+
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
 
 	signals := make(chan os.Signal, 1)
-	isSocketEnabled := make(chan bool, 1)
 	signal.Notify(signals, syscall.SIGTERM)
 
 	// There is an autoincremental msgID to identify every message sent
@@ -75,52 +154,10 @@ func (c *Client) StartClientLoop() {
 			c.config.ID,
 			msgID,
 		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
 
-		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
-			return
-		}
-
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		go func() {
-			select {
-			// Waits a time between sending one message and the next one
-			case currentTime := <-time.After(c.config.LoopPeriod):
-				log.Infof("action: sleep_time | result: success | client_id: %v | msg: awaiked at %v",
-					c.config.ID,
-					currentTime,
-				)
-				isSocketEnabled <- true
-
-			case received_signal := <-signals:
-				log.Debugf("action: sigterm_signal | result: success | client_id: %v | msg: %v signal received",
-					c.config.ID,
-					received_signal,
-				)
-
-				err := c.conn.Close()
-				if err != nil {
-					log.Errorf("action: closing_connection | result: fail | client_id: %v | error: %v",
-						c.config.ID,
-						err,
-					)
-				}
-
-				isSocketEnabled <- false
-			}
-		}()
-
-		c.isEnabled = <-isSocketEnabled
-
+		c.waitServerReponse(signals)
+		c.closeSocketConnection()
+		c.delayMessageDelivery(signals)
 	}
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
